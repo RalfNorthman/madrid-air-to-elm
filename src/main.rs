b1bff15cdacda3b8dbc85afefc212a6d7bf626e1
Ralf Northman <ralf.northman@bredband.net>
2019-05-01 08:04:13 +0200

    Set up the struct for the datapoints.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate csv;
 #[macro_use]
 extern crate serde_derive;
+extern crate chrono;
 
+use chrono::NaiveDateTime;
 use std::error::Error;
 use std::io;
 use std::process;
@@ -10,10 +12,22 @@ use std::process;
 // a corresponding field in the CSV data's header record.
 #[derive(Debug, Deserialize)]
 struct Record {
-    city: String,
-    region: String,
-    country: String,
-    population: Option<u64>,
+    date: NaiveDateTime,
+    BEN: Option<f64>,
+    CH4: Option<f64>,
+    CO: Option<f64>,
+    EBE: Option<f64>,
+    NMHC: Option<f64>,
+    NO: Option<f64>,
+    NO_2: Option<f64>,
+    NOX: Option<f64>,
+    O_3: Option<f64>,
+    PM10: Option<f64>,
+    PM25: Option<f64>,
+    SO_2: Option<f64>,
+    TCH: Option<f64>,
+    TOL: Option<f64>,
+    station: u32,
 }
 
 fn example() -> Result<(), Box<Error>> {
