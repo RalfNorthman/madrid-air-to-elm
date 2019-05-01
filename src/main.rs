63db9a00862074a77a55af8f7099b546c4507482
Ralf Northman <ralf.northman@bredband.net>
2019-05-01 10:47:55 +0200

    Decent elm output. Need to convert date to Posix though.

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,52 @@ use std::process;
 #[derive(Debug, Deserialize)]
 struct Record {
     date: NaiveDateTime,
-    BEN: Option<f64>,
-    CH4: Option<f64>,
-    CO: Option<f64>,
-    EBE: Option<f64>,
-    NMHC: Option<f64>,
-    NO: Option<f64>,
-    NO_2: Option<f64>,
-    NOX: Option<f64>,
-    O_3: Option<f64>,
-    PM10: Option<f64>,
-    PM25: Option<f64>,
-    SO_2: Option<f64>,
-    TCH: Option<f64>,
-    TOL: Option<f64>,
+    ben: Option<f64>,
+    ch4: Option<f64>,
+    co: Option<f64>,
+    ebe: Option<f64>,
+    nmhc: Option<f64>,
+    no: Option<f64>,
+    no_2: Option<f64>,
+    nox: Option<f64>,
+    o_3: Option<f64>,
+    pm10: Option<f64>,
+    pm25: Option<f64>,
+    so_2: Option<f64>,
+    tch: Option<f64>,
+    tol: Option<f64>,
     station: u32,
+}
+
+impl Record {
+    fn to_elm(&self) -> String {
+        format!(
+            ", Record {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}",
+            self.date,
+            to_maybe(self.ben),
+            to_maybe(self.ch4),
+            to_maybe(self.co),
+            to_maybe(self.ebe),
+            to_maybe(self.nmhc),
+            to_maybe(self.no),
+            to_maybe(self.no_2),
+            to_maybe(self.nox),
+            to_maybe(self.o_3),
+            to_maybe(self.pm10),
+            to_maybe(self.pm25),
+            to_maybe(self.so_2),
+            to_maybe(self.tch),
+            to_maybe(self.tol),
+            self.station
+        )
+    }
+}
+
+pub fn to_maybe(opt: Option<f64>) -> String {
+    match opt {
+        None => String::from("Nothing"),
+        Some(f) => format!("(Just {})", f),
+    }
 }
 
 fn example() -> Result<(), Box<Error>> {
@@ -36,7 +67,7 @@ fn example() -> Result<(), Box<Error>> {
         // Notice that we need to provide a type hint for automatic
         // deserialization.
         let record: Record = result?;
-        println!("{:?}", record);
+        println!("{:?}", record.to_elm());
     }
     Ok(())
 }
